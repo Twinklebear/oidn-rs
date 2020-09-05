bf7574a323acb7ee7b3cac38dc49676a134ad038
Moritz Moeller <virtualritz@gmail.com>
2020-09-05 18:10:40 +0200

    Added support for in-place filtering and refactored API to match.

--- src/filter.rs
+++ src/filter.rs
@@ -13,6 +13,8 @@ pub enum FilterError {
 pub struct RayTracing<'a> {
     handle: OIDNFilter,
     device: &'a Device,
+    albedo: Option<&'a [f32]>,
+    normal: Option<&'a [f32]>,
     hdr: bool,
     hdr_scale: f32,
     srgb: bool,
@@ -24,10 +26,12 @@ impl<'a> RayTracing<'a> {
         unsafe {
             oidnRetainDevice(device.handle);
         }
-        let filter = unsafe { oidnNewFilter(device.handle, &b"RT\0" as *const _ as _) };
+        let filter = unsafe { oidnNewFilter(device.handle, b"RT\0" as *const _ as _) };
         RayTracing {
             handle: filter,
             device: device,
+            albedo: None,
+            normal: None,
             hdr: false,
             hdr_scale: std::f32::NAN,
             srgb: false,
@@ -35,66 +39,60 @@ impl<'a> RayTracing<'a> {
         }
     }
 
-    pub fn set_hdr(&mut self, hdr: bool) -> &mut RayTracing<'a> {
+    pub fn albedo_normal(&mut self, albedo: &'a [f32], normal: &'a [f32]) -> &mut RayTracing<'a> {
+        self.albedo = Some(albedo);
+        self.normal = Some(normal);
+        self
+    }
+
+    pub fn albedo(&mut self, albedo: &'a [f32]) -> &mut RayTracing<'a> {
+        self.albedo = Some(albedo);
+        self
+    }
+
+    pub fn hdr(&mut self, hdr: bool) -> &mut RayTracing<'a> {
         self.hdr = hdr;
         self
     }
 
-    pub fn set_hdr_scale(&mut self, hdr_scale: f32) -> &mut RayTracing<'a> {
+    pub fn hdr_scale(&mut self, hdr_scale: f32) -> &mut RayTracing<'a> {
         self.hdr_scale = hdr_scale;
         self
     }
 
-    pub fn set_srgb(&mut self, srgb: bool) -> &mut RayTracing<'a> {
+    pub fn srgb(&mut self, srgb: bool) -> &mut RayTracing<'a> {
         self.srgb = srgb;
         self
     }
 
-    pub fn set_img_dims(&mut self, width: usize, height: usize) -> &mut RayTracing<'a> {
+    pub fn image_dimensions(&mut self, width: usize, height: usize) -> &mut RayTracing<'a> {
         self.img_dims = (width, height);
         self
     }
 
-    pub fn execute(&mut self, color: &[f32], output: &mut [f32]) -> Result<(), FilterError> {
-        self.execute_filter(color, None, None, output)
+    pub fn filter(&self, color: &[f32], output: &mut [f32]) -> Result<(), FilterError> {
+        self.execute_filter(Some(color), output)
     }
 
-    pub fn execute_with_albedo(
-        &mut self,
-        color: &[f32],
-        albedo: &[f32],
-        output: &mut [f32],
-    ) -> Result<(), FilterError> {
-        self.execute_filter(color, Some(albedo), None, output)
-    }
-
-    pub fn execute_with_albedo_normal(
-        &mut self,
-        color: &[f32],
-        albedo: &[f32],
-        normal: &[f32],
-        output: &mut [f32],
-    ) -> Result<(), FilterError> {
-        self.execute_filter(color, Some(albedo), Some(normal), output)
+    pub fn filter_in_place(&self, color: &mut [f32]) -> Result<(), FilterError> {
+        self.execute_filter(None, color)
     }
 
     fn execute_filter(
-        &mut self,
-        color: &[f32],
-        albedo: Option<&[f32]>,
-        normal: Option<&[f32]>,
+        &self,
+        color: Option<&[f32]>,
         output: &mut [f32],
     ) -> Result<(), FilterError> {
         let buffer_dims = 3 * self.img_dims.0 * self.img_dims.1;
 
-        if let Some(alb) = albedo {
+        if let Some(alb) = self.albedo {
             if alb.len() != buffer_dims {
                 return Err(FilterError::InvalidImageDimensions);
             }
             unsafe {
                 oidnSetSharedFilterImage(
                     self.handle,
-                    &b"albedo\0" as *const _ as _,
+                    b"albedo\0" as *const _ as _,
                     alb.as_ptr() as *mut _,
                     Format::FLOAT3,
                     self.img_dims.0,
@@ -107,14 +105,14 @@ impl<'a> RayTracing<'a> {
 
             // No use supplying normal if albedo was
             // not also given.
-            if let Some(norm) = normal {
+            if let Some(norm) = self.normal {
                 if norm.len() != buffer_dims {
                     return Err(FilterError::InvalidImageDimensions);
                 }
                 unsafe {
                     oidnSetSharedFilterImage(
                         self.handle,
-                        &b"normal\0" as *const _ as _,
+                        b"normal\0" as *const _ as _,
                         norm.as_ptr() as *mut _,
                         Format::FLOAT3,
                         self.img_dims.0,
@@ -127,14 +125,25 @@ impl<'a> RayTracing<'a> {
             }
         }
 
-        if color.len() != buffer_dims {
-            return Err(FilterError::InvalidImageDimensions);
-        }
+        let color_ptr = match color {
+            Some(color) => {
+                if color.len() != buffer_dims {
+                    return Err(FilterError::InvalidImageDimensions);
+                }
+                color.as_ptr()
+            }
+            None => {
+                if output.len() != buffer_dims {
+                    return Err(FilterError::InvalidImageDimensions);
+                }
+                output.as_ptr()
+            }
+        };
         unsafe {
             oidnSetSharedFilterImage(
                 self.handle,
-                &b"color\0" as *const _ as _,
-                color.as_ptr() as *mut _,
+                b"color\0" as *const _ as _,
+                color_ptr as *mut _,
                 Format::FLOAT3,
                 self.img_dims.0,
                 self.img_dims.1,
@@ -150,7 +159,7 @@ impl<'a> RayTracing<'a> {
         unsafe {
             oidnSetSharedFilterImage(
                 self.handle,
-                &b"output\0" as *const _ as _,
+                b"output\0" as *const _ as _,
                 output.as_mut_ptr() as *mut _,
                 Format::FLOAT3,
                 self.img_dims.0,
@@ -162,9 +171,9 @@ impl<'a> RayTracing<'a> {
         }
 
         unsafe {
-            oidnSetFilter1b(self.handle, &b"hdr\0" as *const _ as _, self.hdr);
-            oidnSetFilter1f(self.handle, &b"hdrScale\0" as *const _ as _, self.hdr_scale);
-            oidnSetFilter1b(self.handle, &b"srgb\0" as *const _ as _, self.srgb);
+            oidnSetFilter1b(self.handle, b"hdr\0" as *const _ as _, self.hdr);
+            oidnSetFilter1f(self.handle, b"hdrScale\0" as *const _ as _, self.hdr_scale);
+            oidnSetFilter1b(self.handle, b"srgb\0" as *const _ as _, self.srgb);
 
             oidnCommitFilter(self.handle);
             oidnExecuteFilter(self.handle);
