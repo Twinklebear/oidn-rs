3a6d313461d14bb65c921301cd9dd98b1b34a66b
hill <hill@hilll.dev>
2025-02-11 20:09:01 +0000

    check if oidn is already available so OIDN_DIR is technically optional

--- build.rs
+++ build.rs
@@ -3,13 +3,20 @@ use std::path::PathBuf;
 
 fn main() {
     if env::var("DOCS_RS").is_err() {
-        if let Ok(e) = env::var("OIDN_DIR") {
-            let mut oidn_dir = PathBuf::from(e);
-            oidn_dir.push("lib");
-            println!("cargo:rustc-link-search=native={}", oidn_dir.display());
+        if let Ok(dir) = env::var("OIDN_DIR") {
+            let mut lib_path = PathBuf::from(dir);
+            lib_path.push("lib");
+            println!("cargo:rustc-link-search=native={}", lib_path.display());
         } else {
-            println!("cargo:error=Please set OIDN_DIR=<path to OpenImageDenoise install root>");
-            panic!("Failed to find OpenImageDenoise");
+            pkg_config::Config::new()
+                .probe("OpenImageDenoise")
+                .unwrap_or_else(|e| {
+                    println!(
+                        "cargo:error=Could not find OpenImageDenoise via pkg-config: {}",
+                        e
+                    );
+                    panic!("Failed to find OpenImageDenoise");
+                });
         }
         println!("cargo:rerun-if-env-changed=OIDN_DIR");
         println!("cargo:rustc-link-lib=OpenImageDenoise");
